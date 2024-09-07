437a8cabe2e3dbfe58eb5933df0851615c588440
Emanumito <emanuel.lucas098@gmail.com>
2024-09-07 10:47:17 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import random
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get("/teste")
+async def funcaoteste():
+    return {"teste": True, "num aleatorio" : random.randint(0,1000)}
