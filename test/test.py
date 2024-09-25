efbecb59d7f4c078ab07700b22f06a8eb51a375a
Emanumito <emanuel.lucas098@gmail.com>
2024-09-25 15:10:29 -0300

    test2

--- test/test.py
+++ test/test.py
@@ -1,10 +1,8 @@
 from src.main import *
 from unittest.mock import patch
 
-
 def test_root():
     assert root() == {"message": "Hello World"}
-
 
 def test_funcaoteste():
     with patch('random.randint', return_value=123):
